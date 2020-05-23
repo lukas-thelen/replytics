9340b06084e147a5d16d8372014b871f56eb70e4
Flockenkiller <64909809+Flockenkiller@users.noreply.github.com>
2020-05-23 16:24:48 +0200

    Update main.js

--- server/main.js
+++ server/main.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 //import { Follower } from '../imports/api/twitter_follower.js';
 //import { Mentions } from '../imports/api/twitter_mentions.js';
 import { initial } from '../imports/server/twitter.js';
+var Twit = require('twit');
+
 
 Meteor.startup(() => {
 	initial();
