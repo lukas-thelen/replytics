ef7f702bf0b413ce15949ceff8bc2e196b7c3216
Flockenkiller <64909809+Flockenkiller@users.noreply.github.com>
2020-05-23 16:03:51 +0200

    Update twitter.js

--- imports/server/twitter.js
+++ imports/server/twitter.js
@@ -1,12 +1,34 @@
-var Twit = require('twit')
+import { Meteor } from 'meteor/meteor';
+import { Follower } from '../imports/api/follower.js';
+import { Mentions } from '../imports/api/mentions.js';
+var Twit = require('twit');
 
-var twitterAPI = new Twit({
-	consumer_key: "yCR61JPigbhs8tQUDMjy1Bgz3", // API key
-	consumer_secret: "ltkN0xgHBeUX9i3mF1fYIQAgsTNYMUc4H6ZyM7sXEvtgVt9JhT", // API secret
-	access_token: "2909379863-zqmljlTQrLGZsXN4Q6lnvf7yoBZ7K6DjDl3Qm9y",
-	access_token_secret: "KLocQGPdkSY9XI36f45AQGNKQ2xXSQdnoUyzmRJhjxIc7"});
+Meteor.startup(() => {
+  var follower = []
 
-
-function followerCount(){
-	twitterAPI.get('followers/ids', { screen_name:"@FlorianKindler"},  function (err, data, response) {console.log(data)});
+async function getFollowers(){
+	let result = await TwitterAPI.get('followers/ids', { screen_name:"@FlorianKindler"});
+	follower = result.data.ids.length;
+	Follower.insert({count: follower, date: new Date()});
+	console.log(Follower.find({}).fetch())
 }
+
+async function getMentions(){
+	let result = await TwitterAPI.get('statuses/mentions_timeline', { screen_name:"@FlorianKindler"});
+	mentionArray = result.data;
+	for (i=0; i<mentionArray.length; i++){
+		Mentions.insert({
+			date: mentionArray[i].created_at,
+			id01: mentionArray[i].id,
+			id02: mentionArray[i].id_str,
+			content: mentionArray[i].text,
+			unsername: mentionArray[i].user.name,
+		})	
+	}
+	console.log(Mentions.find({}).fetch())
+}
+Follower.remove({});
+getFollowers();
+getMentions();
+
+});
