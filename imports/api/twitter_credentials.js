3e0a69160a933014df02b1f9e7e3e952f9bea9a5
Flockenkiller <64909809+Flockenkiller@users.noreply.github.com>
2020-05-23 16:32:47 +0200

    Update twitter_credentials.js

--- imports/api/twitter_credentials.js
+++ imports/api/twitter_credentials.js
@@ -1,7 +1,7 @@
 var Twit = require('twit');
 
 
-export let TwitterAPI = new Twit({
+export var TwitterAPI = new Twit({
 	consumer_key: "yCR61JPigbhs8tQUDMjy1Bgz3", // API key
 	consumer_secret: "ltkN0xgHBeUX9i3mF1fYIQAgsTNYMUc4H6ZyM7sXEvtgVt9JhT", // API secret
 	access_token: "2909379863-zqmljlTQrLGZsXN4Q6lnvf7yoBZ7K6DjDl3Qm9y",
