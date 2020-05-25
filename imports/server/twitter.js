2793f4a63120cd572250b668a0d0327c8ee01378
Flockenkiller <64909809+Flockenkiller@users.noreply.github.com>
2020-05-25 10:13:04 +0200

    Update twitter.js

--- imports/server/twitter.js
+++ imports/server/twitter.js
@@ -4,29 +4,29 @@ import { Follower } from '../api/twitter_follower.js';
 import { Mentions } from '../api/twitter_mentions.js';
 import { MentionCount } from '../api/twitter_mentionCount.js';
 
-var Twit = require('twit');
+var Twit = require('twit');	//https://github.com/ttezel/twit
 
 
-async function getFollowers(){
+async function getFollowers(){	//Funktion für den Abruf und zur Berechnung der Followeranzahl
 	var follower = []
-	let result = await TwitterAPI.get('followers/ids', { screen_name:"@FlorianKindler"});
-	follower = result.data.ids.length;
-	Follower.insert({count: follower, date: new Date()});
+	let result = await TwitterAPI.get('followers/ids', { screen_name:"@FlorianKindler"});	//Holt die IDs der Follower abhängig von screen_name/Benutzername
+	follower = result.data.ids.length;	//Gibt die Anzahl der Follower anhand der Zahl der Elemente aus
+	Follower.insert({count: follower, date: new Date()});	//Speichert die Followerzahl zusammen mit dem Datum des Abrufs in der Collection "Follower"
 	console.log(Follower.find({}).fetch())
 }
 
 
-async function getMentions(){
-	let result = await TwitterAPI.get('statuses/mentions_timeline', { screen_name:"@FlorianKindler"});
+async function getMentions(){	//Funktion für den Abruf und zur Berechnung der @-Erwähnungen durch andere Benutzer
+	let result = await TwitterAPI.get('statuses/mentions_timeline', { screen_name:"@FlorianKindler"});	//Ruft alle Posts ab die den entsprechenden screen_name/Benutzernamen enthalten
 	mentionArray = result.data;
 	var authorCount = 0
 
-	for (i=0; i<mentionArray.length; i++){
+	for (i=0; i<mentionArray.length; i++){	//Funktion um festzustellen wie viele unterschiedliche Autoren den Benutzer in ihren Posts erwähnen
 		var author = Mentions.find({author: mentionArray[i].user.name}).fetch();
-		if(!author[0]){
+		if(!author[0]){	//Erhöht den Counter, wenn der/die AutorIn bisher noch nicht vorgekommen ist
 			authorCount ++
 		}	
-		Mentions.insert({
+		Mentions.insert({	//Abspeicherung der relevanten Daten in der Collection "Mentions"
 			date: mentionArray[i].created_at,
 			id01: mentionArray[i].id,
 			id02: mentionArray[i].id_str,
@@ -35,7 +35,7 @@ async function getMentions(){
 		})
 
 	}
-	MentionCount.insert({date: new Date(), mentions: mentionArray.length, authors: authorCount})
+	MentionCount.insert({date: new Date(), mentions: mentionArray.length, authors: authorCount}) //Abspeicherung des Datum wann die Daten abgerufen wurden, der Anzahl der Erwähnungen, der Anzahl der verschiedenen Autoren
 	console.log(Mentions.find({}).fetch())
 	console.log(MentionCount.find({}).fetch())
 }
