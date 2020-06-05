70bfe72c0eeed570b2924bf32f979732ecb07b1b
marlahendrich <marlahendrich@gmx.de>
2020-06-05 16:31:21 +0200

    Neu

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,40 +1,60 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import  Tracker  from 'tracker-component';
-
+//Datenbanken
 import { FollowerCount } from '../api/twitter_followerCount.js';
+import { MentionCount } from '../api/twitter_mentionCount.js';
+//Components
+import { KeyFacts } from './KeyFacts.jsx';
+import { FollowerVerlauf } from './FollowerVerlauf';
+
 
 
 // App component - represents the whole app -> alle anderen Components hier ausgeben
 //wird dann gesammelt an main.js geschickt
 class App extends Tracker.Component {
     
-//PROBLEM: Zugriff auf Datenbank ist langsamer als Aufruf der ganzen Funktionen 
-// Rendern muss verzögert werden oder Platzhalter durch automatische updates ausgetauscht werden
+  
+
+ 
+  
+  getMentions() {
+    var mentions = MentionCount.find({}, {sort: {date: -1}}).fetch();
+    return mentions;
+  }
+    
+    
 
     
-    getTodaysFollower(){
-        var follower = [];
-        follower = FollowerCount.find({}, {sort: {date: -1}}).fetch();
-        return follower[0];
-    }
-   
      render() {
-       if(this.getTodaysFollower() != undefined) {
-
+      
+       //Zugriff auf Datenbank ist langsamer als Aufruf der ganzen Funktionen 
+       //Rendern muss verzögert werden oder Platzhalter durch automatische updates ausgetauscht werden
+       //if-Bedingung wichtig, um Fehlermeldungen zu vermeiden während die Daten laden
+       
+        if (this.getMentions()[0] != undefined ){
            return(
-                <p> {this.getTodaysFollower().count} </p>
-           );
-        
-       } else {
 
-        return (
-          <div>
-          <p> Daten werden geladen, bitte warten. </p>
-           
-           </div>
-        );
-       }
+             <div>
+                
+                <KeyFacts 
+                  mentionCount={this.getMentions()[0].mentions}
+                  mentionAuthors={this.getMentions()[0].authors}                  
+                />
+
+              
+               
+                
+                
+              </div>
+           );
+          } else  {
+
+            return (
+              <p>  </p>
+            );
+          }
+       
     }
   }
   
