516dcdc2353f2113614c2e7b86d19c7ee818f867
lukas-thelen <lukas.thelen@t-online.de>
2020-07-02 18:42:01 +0200

    auberer Code

--- imports/ui/navbar.jsx
+++ imports/ui/navbar.jsx
@@ -60,7 +60,7 @@ export class Navbar extends Tracker.Component {
                             </button>
                             <div className="dropdown-menu" aria-labelledby="dropdownSettingsButton">
 
-                                <a className="dropdown-item" href="#" onClick={this.props.twitter_authorization}>Twitter autorisieren</a>
+                                <a style={{fontSize:14}} className="dropdown-item border-bottom" href="#" onClick={this.props.twitter_authorization}>Twitter autorisieren</a>
                                 <a style={{fontSize:14}} className="dropdown-item border-bottom" href="#" onClick={this.props.goToSettings}>Einstellungen</a>
                                   <a style={{fontSize:14}} className="dropdown-item border-bottom" href="#" onClick={this.props.goToHelp}>Hilfe</a>
                                 <a style={{fontSize:14}} className="dropdown-item" href="#">Ausloggen</a>
