8a0ddbc48cf58641822fb3dfbdf0209f91d321b2
MariaHeiland <64899878+MariaHeiland@users.noreply.github.com>
2020-07-03 10:32:58 +0200

    Posten in Reddit

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -16,6 +16,7 @@ import { DimensionenRadar } from './Dimensionen.jsx';
 import { Settings } from './Settings.jsx';
 import { SearchPosts } from './SearchPosts.jsx';
 
+
 import AccountsUIWrapper from './AccountsUIWrapper.js';
 import { Navbar } from './navbar.jsx';
 import { Benachrichtigungen } from './Benachrichtigungen.jsx';
@@ -138,8 +139,9 @@ class App extends Tracker.Component {
 
                   <div className="col-xl-5 elem erste">
                     
+                    
                     <Selbstposten/>
-                    <Benachrichtigungen/>
+					<Benachrichtigungen/>
                     <br/>
                     <div className="d-flex w-100 justify-content-between " >
                       <h5 style={{display:"inline"}} >Post-Analyse
@@ -147,6 +149,7 @@ class App extends Tracker.Component {
                         <path fillRule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                         <path d="M5.25 6.033h1.32c0-.781.458-1.384 1.36-1.384.685 0 1.313.343 1.313 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.007.463h1.307v-.355c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.326 0-2.786.647-2.754 2.533zm1.562 5.516c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
                       </svg></button></h5>
+					  
                       <span className="btn-group btn-group-sm" role="group" aria-label="Basic example">
                         <button type="button" className="btn " onClick={this.toTop}>Top Posts</button>
                         <button type="button" className="btn " onClick={this.toPop}>Posts suchen</button>
@@ -158,18 +161,12 @@ class App extends Tracker.Component {
 
                   <div className="col-xl-7 row">
                     <div className="col-md-6 elem zweite">
-                      Kategorien
+                     
 					            <DimensionenRadar/>
                       <br/>
                       <br/>
 					            <BarChart/>
-					            <div className="info text-bottom spalte">
-					              Fragen? Dann schreiben Sie uns!
-					              <br/>
-				              	Telefonnummer: 08001233477
-					              <br/>
-					              E-Mail: replytics@hotmail.de
-                      </div>
+					        
 
 					          </div>
                     <div className="col-md-6 elem dritte">
